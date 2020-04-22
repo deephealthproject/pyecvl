77a7f1e462c6fee2208e1b14600d41b8b8936463
simleo <simleo@crs4.it>
2020-04-22 17:03:33 +0200

    dataset_parser docs

--- pyecvl/ecvl.py
+++ pyecvl/ecvl.py
@@ -723,19 +723,80 @@ def MeanStdDev(src):
 # == dataset_parser ==
 
 class SplitType(_ecvl.SplitType):
-    pass
+    """\
+    Enum class representing the supported split types.
+    """
+    training = _ecvl.SplitType.training
+    validation = _ecvl.SplitType.validation
+    test = _ecvl.SplitType.test
 
 
 class Sample(_ecvl.Sample):
-    pass
+    r"""\
+    Sample image in a dataset.
+
+    Provides the information to describe a dataset sample.
+
+    :var location\_: absolute path(s) of the sample (list of strings)
+    :var label_path\_: absolute path of the sample's ground truth
+    :var label\_: sample labels (list of integers)
+    :var values\_: feature index-to-value mapping
+    """
+    def LoadImage(self, ctype=ColorType.BGR, is_gt=False):
+        """\
+        Return the dataset image for this sample.
+
+        Opens the sample image from ``location_`` or ``label_path_``, depending
+        on the ``is_gt`` parameter.
+
+        :param ctype: ColorType of the returned image
+        :param is_gt: whether to load the sample image or its ground truth
+        :return: sample image
+        """
+        return _ecvl.Sample.LoadImage(self, ctype, is_gt)
 
 
 class Split(_ecvl.Split):
-    pass
+    r"""\
+    Provides the splits a dataset can have: training, validation, and test.
+
+    :var training\_: training samples indices (list of integers)
+    :var validation\_: validation samples indices (list of integers)
+    :var test\_: test samples indices (list of integers)
+    """
 
 
 class Dataset(_ecvl.Dataset):
-    pass
+    r"""\
+    DeepHealth Dataset.
+
+    Implements the `DeepHealth Dataset Format
+    <https://github.com/deephealthproject/ecvl/wiki/DeepHealth-Toolkit-Dataset-Format>`_.
+
+    :var name\_: dataset name
+    :var description\_: dataset description
+    :var classes\_: classes available in the dataset (list of strings)
+    :var features\_: features available in the dataset (list of strings)
+    :var samples\_: list of dataset samples
+    :var split\_: dataset splits
+    """
+    def __init__(self, filename):
+        """\
+        :param filename: path to the dataset file
+        """
+        _ecvl.Dataset.__init__(self, filename)
+
+    def Dump(self, path):
+        """\
+        Dump the dataset to YAML following the DeepHealth Dataset Format.
+
+        The YAML file is saved into the dataset's root directory. Sample paths
+        are relative to the dataset's root directory.
+
+        :param path: output file path
+        :return: None
+        """
+        _ecvl.Dataset.Dump(self, path)
 
 
 # == augmentations ==
