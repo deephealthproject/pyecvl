9e58241cf1517d781e5d164be3c44fbd910ee88f
simleo <simleo@crs4.it>
2020-04-23 16:04:10 +0200

    more docstrings

--- pyecvl/ecvl.py
+++ pyecvl/ecvl.py
@@ -974,34 +974,185 @@ class AugCoarseDropout(_ecvl.AugCoarseDropout):
 # == support_imgcodecs ==
 
 class ImReadMode(_ecvl.ImReadMode):
-    pass
+    """\
+    Enum class representing the possible image read modes.
+    """
+    GRAYSCALE = _ecvl.ImReadMode.GRAYSCALE
+    COLOR = _ecvl.ImReadMode.COLOR
 
 
 def ImRead(filename, flags=ImReadMode.COLOR):
+    """\
+    Load an image from a file.
+
+    :param filename: name of the input file
+    :param flags: an ImReadMode indicating how to read the image
+    :return: an Image object
+    """
     return _ecvl.ImRead(filename, flags)
 
 
 def ImWrite(filename, src):
+    """\
+    Save an image to a file.
+
+    The image format is chosen based on the filename extension.
+
+    :param filename: name of the output file
+    :param src: Image to be saved
+    :return: None
+    """
     return _ecvl.ImWrite(filename, src)
 
 
 # == support_eddl ==
 
 class DatasetAugmentations(_ecvl.DatasetAugmentations):
-    pass
+    """\
+    Represents the augmentations which will be applied to each split.
+    """
+    def __init__(self, augs):
+        """\
+        :param augs: three-element list containing the augmentations to be \
+          applied to the training, validation and test splits respectively. \
+          If no augmentations need to be defined for a split, set the \
+          corresponding list element to None.
+        """
+        _ecvl.DatasetAugmentations.__init__(self, augs)
+
+    def Apply(self, st, img, gt=None):
+        """\
+        Apply augmentations for the specified split to an image.
+
+        :param st: a SplitType specifying which set of augmentations
+          should be applied
+        :param img: image to which augmentations should be applied
+        :param gt: ground truth image to which augmentations should be applied
+        :return: None
+        """
+        if gt is None:
+            return _ecvl.DatasetAugmentations.Apply(self, st, img)
+        return _ecvl.DatasetAugmentations.Apply(self, st, img, gt)
 
 
 class DLDataset(_ecvl.DLDataset):
-    pass
+    r"""\
+    DeepHealth deep learning dataset.
+
+    :var batch_size\_: size of each dataset mini batch
+    :var n_channels\_: number of image channels
+    :var n_channels_gt\_: number of ground truth image channels
+    :var current_split\_: = Current SplitType from which images are loaded
+    :var resize_dims\_: dimensions ``[H, W]`` to which images must be resized
+    :var current_batch\_: number of batches already loaded for each split
+    :var ctype\_: ColorType of the dataset images
+    :var ctype_gt\_: ColorType of the dataset ground truth images
+    :var augs\_: DatasetAugmentations to be applied to the dataset images
+      (and ground truth if existing) for each split
+    """
+    def __init__(self, filename, batch_size, augs=None, ctype=ColorType.BGR,
+                 ctype_gt=ColorType.GRAY, verify=False):
+        """\
+        :param filename: path to the dataset file
+        :param batch_size: size of each dataset mini batch
+        :param augs: a DatasetAugmentations object specifying the training,
+          validation and test augmentations to be applied to the dataset images
+          (and ground truth if existing) for each split. Set to None if no
+          augmentation is required
+        :param ctype: ColorType of the dataset images
+        :param ctype_gt: ColorType of the dataset ground truth images
+        :param verify: if True, verify image existence
+        """
+        if augs is None:
+            augs = _ecvl.DatasetAugmentations()
+        _ecvl.DLDataset.__init__(
+            self, filename, batch_size, augs, ctype, ctype_gt
+        )
+
+    def GetSplit(self, split=None):
+        """\
+        Get the image indexes for a split.
+
+        :param split: a SplitType; if None, will default to the current split
+        :return: list of image indexes for the split
+        """
+        if split is None:
+            return _ecvl.DLDataset.GetSplit(self)
+        return _ecvl.DLDataset.GetSplit(self, split)
+
+    def ResetCurrentBatch(self):
+        """\
+        Reset the batch counter of the current split.
+        :return: None
+        """
+        return _ecvl.DLDataset.ResetCurrentBatch(self)
+
+    def ResetAllBatches(self):
+        """\
+        Reset the batch counter of each split.
+        :return: None
+        """
+        return _ecvl.DLDataset.ResetAllBatches(self)
+
+    def SetSplit(self, split):
+        """\
+        Set the current Split.
+
+        :param split: a SplitType
+        :return: None
+        """
+        return _ecvl.DLDataset.SetSplit(self, split)
+
+    def LoadBatch(self, images, labels=None):
+        """\
+        Load a batch into the images and labels tensors.
+
+        :param images: a Tensor to store the batch of images
+        :param labels: a Tensor to store the batch of labels
+        """
+        if labels is None:
+            return _ecvl.DLDataset.LoadBatch(self, images)
+        return _ecvl.DLDataset.LoadBatch(self, images, labels)
 
 
+# FIXME: the version with offset does not make sense, since we are returning
+# the output tensor
 def ImageToTensor(img, offset=None):
+    """\
+    Insert an ECVL Image into an EDDL Tensor.
+
+    The input image must have 3 dimensions "xy[czo]" (in any order). The output
+    tensor's dimensions will be C x H x W, where:
+
+    * C = channels
+    * H = height
+    * W = width
+
+    :param img: input image
+    :param offset: how many images are already stored in the tensor
+    :return: output tensor
+    """
     if offset is None:
         return _ecvl.ImageToTensor(img)
     return _ecvl.ImageToTensor(img, offset)
 
 
 def TensorToImage(t):
+    """\
+    Convert an EDDL Tensor into an ECVL Image.
+
+    Tensor dimensions must be C x H x W or N x C x H x W, where:
+
+    * N = batch size
+    * C = channels
+    * H = height
+    * W = width
+
+    The output image will be "xyo" with DataType.float32 and ColorType.none.
+
+    :param t: input tensor.
+    :return: output image
+    """
     return _ecvl.TensorToImage(t)
 
 
@@ -1012,30 +1163,74 @@ def TensorToView(t):
 # == support_openslide ==
 
 def OpenSlideGetLevels(filename):
+    """\
+    Get the width and height for each level of a whole-slide image.
+
+    :param filename: image file name
+    :return: a list of pairs representing the width and height of each level
+    """
     return _ecvl.OpenSlideGetLevels(filename)
 
 
 def OpenSlideRead(filename, level, dims):
+    """\
+    Load a region of a whole-slide image.
+
+    Supported formats are those supported by the OpenSlide library.
+
+    :param filename: image file name
+    :param level: image level to extract
+    :param dims: ``[x, y, w, h]`` list representing the region to extract.
+      ``x`` and ``y`` are the top-left x and y coordinates in the level 0
+      reference frame. ``w`` and ``h`` are the width and height of the region
+    :return: an Image object
+    """
     return _ecvl.OpenSlideRead(filename, level, dims)
 
 
 # == support_nifti ==
 
 def NiftiRead(filename):
+    """\
+    Loads a nifti image from a file.
+
+    :param filename: image file name
+    :return: an Image object
+    """
     return _ecvl.NiftiRead(filename)
 
 
 def NiftiWrite(filename, src):
+    """\
+    Save an image to a file in the NIfTI-1 format.
+
+    :param filename: image file name
+    :param src: image to be saved
+    :return: None
+    """
     return _ecvl.NiftiWrite(filename, src)
 
 
 # == support_dcmtk ==
 
 def DicomRead(filename):
+    """\
+    Loads a dicom image from a file.
+
+    :param filename: image file name
+    :return: an Image object
+    """
     return _ecvl.DicomRead(filename)
 
 
 def DicomWrite(filename, src):
+    """\
+    Save an image to a file in the DICOM format.
+
+    :param filename: image file name
+    :param src: image to be saved
+    :return: None
+    """
     return _ecvl.DicomWrite(filename, src)
 
 
