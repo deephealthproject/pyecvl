806b1d81df3e3ad690e1d10e1b6baf4eea412132
simleo <simleo@crs4.it>
2020-10-14 11:44:41 +0200

    updated to pyeddl 0.9.0

--- build_docker_gpu.sh
+++ build_docker_gpu.sh
@@ -2,13 +2,9 @@
 
 set -euo pipefail
 
-# Current Dockerfile.eddl-gpu in pyeddl has gcc 5 and cmake 3.14, which are
-# too old for ecvl.
-
-# pushd third_party/pyeddl
-# docker build -t eddl-gpu -f Dockerfile.eddl-gpu .
-# popd
+pushd third_party/pyeddl
 docker build -t eddl-gpu -f Dockerfile.eddl-gpu .
+popd
 
 docker build -t ecvl-gpu -f Dockerfile.ecvl-gpu .
 docker build -t pyecvl-gpu -f Dockerfile.gpu .
