a18a987121cc5e592ac9446993d25f494c3d4292
simleo <simleo@crs4.it>
2020-11-30 15:32:21 +0100

    update jenkins image tag

--- build_jenkins_image.sh
+++ build_jenkins_image.sh
@@ -11,7 +11,12 @@ ecvl_rev=$(git rev-parse --short HEAD)
 popd
 pushd third_party/pyeddl
 pyeddl_rev=$(git rev-parse --short HEAD)
+pushd third_party/eddl
+eddl_rev=$(git rev-parse --short HEAD)
+[ "${eddl_rev}" = "c023a6e" -o "${eddl_rev}" = "01ef395" ] && git apply ../../eddl_0.3.patch
 popd
-docker build -t simleo/ecvl:${ecvl_rev}-${pyeddl_rev} -f Dockerfile.ecvl .
+docker build -t eddl:${eddl_rev} -f Dockerfile.eddl .
+popd
+docker build --build-arg eddl_rev=${eddl_rev} -t simleo/ecvl:${ecvl_rev}-${pyeddl_rev} -f Dockerfile.ecvl .
 docker build -f Dockerfile.jenkins --build-arg ecvl_rev=${ecvl_rev} --build-arg pyeddl_rev=${pyeddl_rev} -t simleo/pyecvl-base:${ecvl_rev}-${pyeddl_rev} .
 popd
